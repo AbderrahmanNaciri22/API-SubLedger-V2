e30441c4b4fa9fa5e45a273bb962609887e03f04
AbderrahmanNaciri22 <abderrhmanen2205@gmail.com>
2026-03-18 10:38:26 +0000

    add getTransactionsByUserId

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute, requireAdmin } from "../middlewares/authMiddleware.js";
-import { getUserSubscriptions ,getUsers,getUsersById } from "../controllers/admin.controller.js";
+import { getUserSubscriptions ,getUsers,getUsersById , getTransactionsByUserId} from "../controllers/admin.controller.js";
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.get("/subscriptions/:id",protectRoute,requireAdmin,getUserSubscriptions,)
 router.get("/users",protectRoute,requireAdmin,getUsers);
 router.get("/users",protectRoute,requireAdmin,getUsers);
 router.get("/users/:id",protectRoute,requireAdmin,getUsersById);
-router.get("/users/:id/transactions",protectRoute,requireAdmin,getUsersById);
+router.get("/users/:id/transactions",protectRoute,requireAdmin,getTransactionsByUserId);
 
 
 
