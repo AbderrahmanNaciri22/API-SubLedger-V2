c0d0ea795f6f200d652d4e8f4f23bc08a19b8b60
walidnoussir <noussirw@gmail.com>
2026-03-18 11:32:56 +0000

    add pagination and filtring

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -25,9 +25,55 @@ export const createSubscription = async (req, res) => {
 // get all subscriptions
 export const getSubscriptions = async (req, res) => {
   try {
-    const subscriptions = await Subscription.find({ userId: req.user._id });
+    const {
+      page = 1,
+      limit = 10,
+      status,
+      subscriptionId,
+      date,
+      startDate,
+      endDate,
+    } = req.query;
 
-    res.status(200).json(subscriptions);
+    const query = { userId: req.user._id };
+
+    // Filter by status
+    if (status) query.status = status;
+
+    // Filter by subscriptionId
+    if (subscriptionId) query._id = subscriptionId;
+
+    // Filter by date
+    if (date) {
+      const filtredDate = new Date(date);
+      filtredDate.setHours(0, 0, 0, 0);
+      const nextDay = new Date(filtredDate);
+      nextDay.setDate(nextDay.getDate() + 1);
+      query.createdAt = { $gte: filtredDate, $lt: nextDay };
+    }
+
+    if (startDate || endDate) {
+      query.createdAt = {};
+      if (startDate) query.createdAt.$gte = new Date(startDate);
+      if (endDate) query.createdAt.$lte = new Date(endDate);
+    }
+
+    // Pagination
+    const skip = (page - 1) * limit;
+
+    const subscriptions = await Subscription.find(query)
+      .sort({ createdAt: -1 })
+      .skip(parseInt(skip))
+      .limit(parseInt(limit));
+
+    const totalSubscriptions = await Subscription.countDocuments(query);
+
+    res.status(200).json({
+      data: subscriptions,
+      totalSubscriptions,
+      totalPages: Math.ceil(totalSubscriptions / limit),
+      currentPage: parseInt(page),
+    });
   } catch (error) {
     console.log("Error on getSubscriptions controller", error);
     res.status(500).json({ message: "Internal server error." });
